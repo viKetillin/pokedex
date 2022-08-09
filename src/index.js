bbfbbeb7a84e8c0960ef4a374d2f51c1038b8aa2
viKetillin <vitoriaketillin@hotmail.com>
2022-08-09 19:50:49 -0300

    Utilizando sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./css/styles.css"
+import "./css/styles.scss"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
